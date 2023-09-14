f506a1633f1fde98acfc7312a6e4378c17e22ca0
Gaxx <damn.frank2z@gmail.com>
2023-09-13 23:06:46 -0700

    init main.rs

--- samples/system_info_plugin/src/main.rs
+++ samples/system_info_plugin/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::BTreeMap;
+use zellij_tile::prelude::*;
+
+#[derive(Default)]
+struct State {}
+
+register_plugin!(State);
+
+impl ZellijPlugin for State {
+    fn load(&mut self, configuration: BTreeMap<String, String>) {
+        println!("hello world!");
+    }
+    fn update(&mut self, event: Event) -> bool {
+        true
+    } // return true if it should render
+    fn render(&mut self, rows: usize, cols: usize) {
+        println!("hello world!");
+    }
 }
