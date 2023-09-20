f9385d77b1258fca89910af9f033f0f8ecf9f8fc
Gaxx <damn.frank2z@gmail.com>
2023-09-19 22:00:53 -0700

    impl default plugin trait

--- samples/float-pane-sized/src/main.rs
+++ samples/float-pane-sized/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use zellij_tile::prelude::*;
+
+use std::collections::BTreeMap;
+
+#[derive(Default)]
+struct State {
+    panes: Panes,
 }
+
+register_plugin!(State);
+
+impl ZellijPlugin for State {
+    fn load(&mut self, _configuration: BTreeMap<String, String>) {
+        subscribe(&[EventType::SessionUpdate]);
+    }
+
+    fn update(&mut self, event: Event) -> bool {
+        let mut render = false;
+        match event {
+            Event::SessionUpdate(session_info) => {
+                self.panes = get_panes(session_info);
+                render = true;
+            }
+            _ => {}
+        }
+        render
+    }
+
+    fn render(&mut self, _rows: usize, _cols: usize) {}
+}
+
+fn get_panes(session: Vec<SessionInfo>) -> Panes {
+    let current_session = session
+        .into_iter()
+        .find(|session| session.is_current_session)
+        .unwrap();
+    todo!();
+}
+
+#[derive(Default)]
+struct Panes {}
